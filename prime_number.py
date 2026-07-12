9ab8fe5c70d205fc8e7ab9f943862ab37b00b2f8
Shrijal Dahal <np02cs4s250007@bicnepal.edu.np>
2026-07-12 18:29:13 +0545

    prime number is done

--- prime_number.py
+++ prime_number.py
@@ -1,9 +1,8 @@
-number = int(input("Enter a number to check if it is prime number or not: "))
-if number > 1: # checks if the user input is greater than 1 or not  if greater than 1 then it will check for prime or not!!
-    for i in range(2, int(number/2)+1):
-        if (number % i) == 0:
-
-            print(number, "is not a prime number")
+for numbers in range (2, 101):
+    for i in range(2,int(numbers**0.5)+1):
+        if numbers % i ==0:
             break
     else:
-        print(number, "is a prime number")
+          print(numbers)
+
+            
